dbf03d836d3db5ce3f73c548f8e1109a5455d268
Aadhav Vijaya Saravanan <144838207+Aadhav-VijayaSaravanan@users.noreply.github.com>
2023-11-10 15:22:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
-import api
+import api  # Import your own API module if needed
 
-def main():
-  inputi = input(
-      "\033[1mCurrency Conversion\033[0m\nEnter the currency you want to convert: "
-  ).upper().split()
-  primary = inputi[1]
-  secondary = inputi[3]
+def main(data):
+    inputi = input(
+        "\033[1mCurrency Conversion\033[0m\nEnter the currency you want to convert: "
+    ).upper().split()
+    primary = inputi[1]
+    secondary = inputi[3]
 
-  if inputi[0].isnumeric:
-    if isinstance(float(inputi[0]), float) or isinstance(int(inputi[0]), int):
-      quantity = float(inputi[0])
-  else:
-    print(
-        "Tring to do something?\n input only numeric digits not in alphabetical form"
-    )
+    if inputi[0].isnumeric():
+        if isinstance(float(inputi[0]), float) or isinstance(int(inputi[0]), int):
+            quantity = float(inputi[0])
+        else:
+            print("Trying to do something? Input only numeric digits, not in alphabetical form")
+            return
 
-  if inputi[1] in api.data["rates"] and inputi[3] in api.data["rates"]:
-    primary_rate = float(api.data["rates"].get(primary))
-    secondary_rate = float(api.data["rates"].get(secondary))
-    coversion_rate = float(quantity * (secondary_rate / primary_rate))
-    print(f"The conversion rate for {primary} is {coversion_rate:.2f}")
-  else:
-    print("your currency isnt valid, try again!")
+    if primary in data["rates"] and secondary in data["rates"]:
+        primary_rate = data["rates"][primary]
+        secondary_rate = data["rates"][secondary]
+        conversion_rate = quantity * (secondary_rate / primary_rate)
+        print(f"The conversion rate for {primary} is {conversion_rate:.2f}")
+    else:
+        print("Your currency isn't valid, please try again!")
 
-  sorte = sorted(api.data["rates"].items(), key=lambda x: float(x[1]), reverse=False)
-  for i in range(len(sorte)):
-    if secondary in sorte[i]:
-      rem = i % 10
-      if rem == 1:
-        fini = "st"
-      elif rem == 2:
-        fini = "nd"
-      elif rem == 3:
-        fini = "rd"
-      else:
-        fini = "th"
-      print(f"{secondary} is the {i}{fini} strongest currency in the world(this also includes crypto currencies!))
+    rates_sorted = sorted(data["rates"].items(), key=lambda x: x[1], reverse=False)
+    for i, rate in enumerate(rates_sorted):
+        currency, rate_value = rate
+        if secondary == currency:
+            ordinal_indicator = "th" if 11 <= i + 1 <= 13 else {1: "st", 2: "nd", 3: "rd"}.get((i + 1) % 10, "th")
+            print(f"{secondary} is the {i + 1}{ordinal_indicator} strongest currency in the world (including cryptocurrencies).")
+            break
+
+if __name__ == "__main__":
+    main(api.data)
